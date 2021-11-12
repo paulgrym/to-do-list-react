cdcb5605b80091dafbf6b9d2546b9361083bd373
Paulina Grymulska <paulina.grymulska@gmail.com>
2021-11-12 22:53:32 +0100

    tasks list without interactions yet

---
+++ src/Buttons/index.js
@@ -0,0 +1,22 @@
+import "./style.css";
+
+const Buttons = ({ tasks, hideDoneTasks }) => {
+    if (tasks.lenght === 0) {
+        return null
+    }
+
+    return (
+        <div className="buttonsContainer">
+            <button className="buttonsContainer__button">
+                {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
+            </button>
+            <button className=
+                "buttonsContainer__button"
+                disabled={tasks.every(({ done }) => done)}>
+                Ukończ wszystkie
+            </button>
+        </div>
+    )
+};
+
+export default Buttons;
---
+++ src/Tasks/index.js
@@ -0,0 +1,23 @@
+import "./style.css";
+
+const Tasks = (props) => (
+    <ul className="list">
+        {props.tasks.map(task => (
+            <li
+                key={task.id}
+                className={`list__item ${task.done && props.hideDoneTasks ? "list__item--hidden" : ""}`}>
+                <button className="list__button">
+                    {task.done ? "✓" : ""}
+                </button>
+                <span className={`list__taskContent ${task.done ? "list__taskContent--done" : ""}`}>
+                    {task.content}
+                </span>
+                <button className="list__button list__button--remove">
+                    🗑
+                </button>
+            </li>
+        ))}
+    </ul>
+);
+
+export default Tasks;
