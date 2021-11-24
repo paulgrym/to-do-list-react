a3d6072f53dc1e9a3e1ce7bbb0bcbcc8627f23fd
Paulina Grymulska <paulina.grymulska@gmail.com>
2021-11-24 18:28:14 +0100

    adding new tasks added

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,10 +1,30 @@
+import { useState } from "react";
 import "./style.css";
 
-const Form = () => (
-    <form className="form">
-        <input className="form__input" placeholder="Co jest do zrobienia?" />
-        <button className="form__button">Dodaj zadanie</button>
-    </form>
-);
+const Form = ({ addNewTask }) => {
+
+    const [newTaskContent, setNewTaskContent] = useState("");
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        const trimmedNewTaskContent = newTaskContent.trim();
+        if (trimmedNewTaskContent === "") {
+            return;
+        };
+        addNewTask(trimmedNewTaskContent);
+        setNewTaskContent("");
+    };
+
+    return (
+        <form className="form" onSubmit={onFormSubmit}>
+            <input
+                value={newTaskContent}
+                onChange={(event) => setNewTaskContent(event.target.value)}
+                className="form__input"
+                placeholder="Co jest do zrobienia?" />
+            <button className="form__button">Dodaj zadanie</button>
+        </form>
+    )
+};
 
 export default Form;
