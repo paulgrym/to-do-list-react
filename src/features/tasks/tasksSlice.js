aadebe3b4a5c2e7deec4bb4bb167f1a22d15ddc7
Paulina Grymulska <paulina.grymulska@gmail.com>
2022-01-19 15:44:31 +0100

    payloads names added

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -7,22 +7,22 @@ const tasksSlice = createSlice({
     hideDone: false,
   },
   reducers: {
-    addTask: (state, action) => {
-      state.tasks.push(action.payload);
+    addTask: ({ tasks }, { payload: newTask }) => {
+      tasks.push(newTask);
     },
     toggleHideDone: state => {
       state.hideDone = !state.hideDone;
     },
-    toggleTaskDone: (state, action) => {
-      const index = state.tasks.findIndex(task => task.id === action.payload);
-      state.tasks[index].done = !state.tasks[index].done;
+    toggleTaskDone: ({ tasks }, { payload: taskId }) => {
+      const index = tasks.findIndex(task => task.id === taskId);
+      tasks[index].done = !tasks[index].done;
     },
-    removeTask: (state, action) => {
-      const index = state.tasks.findIndex(task => task.id === action.payload);
-      state.tasks.splice(index, 1);
+    removeTask: ({ tasks }, { payload: taskId }) => {
+      const index = tasks.findIndex(task => task.id === taskId);
+      tasks.splice(index, 1);
     },
-    setAllDone: (state) => {
-      for (const task of state.tasks) {
+    setAllDone: ({ tasks }) => {
+      for (const task of tasks) {
         task.done = true
       }
     },
